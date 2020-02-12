c0c4259e8ee33b5a5d6c21dda2b8b436f45ff621
Steve Yang <steve.yang@gov.ab.ca>
2020-02-11 17:03:21 -0700

    api

--- helloApi/helloApi/settings.py
+++ helloApi/helloApi/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',    
+    'snippets.apps.SnippetsConfig',    
 ]
 
 MIDDLEWARE = [
@@ -106,9 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20
 }
--- helloApi/snippets/serializers.py
+++ helloApi/snippets/serializers.py
@@ -1,14 +1,50 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'groups']
 
 
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
+class GroupSerializer(serializers.ModelSerializer): #HyperlinkedModelSerializer = ModelSerializer ?
     class Meta:
         model = Group
         fields = ['url', 'name']
+#modal and fields        
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+'''
+# SnippetSerializer(model object/python object), #.data, is_valid(), .validated_data .save()
+# serialize:  1. SnippetSerializer(model), 2. JSONRenderer().render(serializer.data)
+# deserialize 1. JasonString b string, 2 into stream, 3.JSONParser().parse(stream) 4.SnippetSerializer(data=data), is_valid(), .validated_data .save()
+class SnippetSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    code = serializers.CharField(style={'base_template': 'textarea.html'})
+    linenos = serializers.BooleanField(required=False)
+    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
+    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+
+    def create(self, validated_data):
+        """
+        Create and return a new `Snippet` instance, given the validated data.
+        """
+        return Snippet.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `Snippet` instance, given the validated data.
+        """
+        instance.title = validated_data.get('title', instance.title)
+        instance.code = validated_data.get('code', instance.code)
+        instance.linenos = validated_data.get('linenos', instance.linenos)
+        instance.language = validated_data.get('language', instance.language)
+        instance.style = validated_data.get('style', instance.style)
+        instance.save()
+        return instance       
+        '''
