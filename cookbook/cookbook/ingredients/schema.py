1795c8c0652fec19e20df0f51d64326e0f192912
Steve Yang <steve.yang@gov.ab.ca>
2020-03-04 16:51:16 -0700

    mutation

--- cookbook/cookbook/ingredients/schema.py
+++ cookbook/cookbook/ingredients/schema.py
@@ -1,17 +1,23 @@
 import graphene
-from graphene import relay, ObjectType
+from graphene import relay, ObjectType, Mutation
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 
 from cookbook.ingredients.models import Category, Ingredient
 
-
 # Graphene will automatically map the Category model's fields onto the CategoryNode.
 # This is configured in the CategoryNode's Meta class (as you can see below)
+
+
+class CategoryType(DjangoObjectType):
+    class Meta:
+        model = Category
+
+
 class CategoryNode(DjangoObjectType):
     class Meta:
         model = Category
-        filter_fields = ['name', 'ingredients']
+        filter_fields = ['name']
         interfaces = (relay.Node, )
 
 
@@ -28,9 +34,29 @@ class IngredientNode(DjangoObjectType):
         interfaces = (relay.Node, )
 
 
-class Query(graphene.ObjectType):
+class CategoryMutation(Mutation):
+
+    class arguments:
+        name = graphene.String(required=True)
+        id = graphene.ID()
+
+    category = graphene.Field(CategoryType)
+
+    @classmethod
+    def mutate(cls, info, name, id):
+        category = Category.objects.get(id=id)
+        category.name = name
+        category.save()
+        return CategoryMutation(category=category)
+
+
+class Query(ObjectType):
     category = relay.Node.Field(CategoryNode)
     all_categories = DjangoFilterConnectionField(CategoryNode)
 
     ingredient = relay.Node.Field(IngredientNode)
     all_ingredients = DjangoFilterConnectionField(IngredientNode)
+
+
+class Matation(ObjectType):
+    update_category = CategoryMutation.Field()
--- cookbook/cookbook/schema.py
+++ cookbook/cookbook/schema.py
@@ -8,4 +8,11 @@ class Query(cookbook.ingredients.schema.Query, graphene.ObjectType):
     # as we begin to add more apps to our project
     pass
 
+
+class Mutation(cookbook.ingredients.schema.Mutation, graphene.ObjectType):
+    # This class will inherit from multiple Queries
+    # as we begin to add more apps to our project
+    pass
+
+
 schema = graphene.Schema(query=Query)
