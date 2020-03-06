987418ce5f758cf1743e4b36e2a5dbdaea487044
Steve Yang <steve.yang@gov.ab.ca>
2020-03-06 10:46:45 -0700

    Add more mutation test

--- cookbook/cookbook/ingredients/TestIngredient.py
+++ cookbook/cookbook/ingredients/TestIngredient.py
@@ -10,7 +10,34 @@ class TestIngredient(GraphQLTestCase):
     # GraphQLTestCase not working
 
     def test_query(self):
-        res = self.query(query='{allCategories{pageInfo {hasNextPage}}}'
-                         # ,op_name='allCategories'
+        res = self.query(query='''query getCategory {
+            allCategories{
+                pageInfo {
+                    hasNextPage
+                    }
+                }
+            } 
+        ''', op_name='getCategory'
+                         )
+        self.assertResponseNoErrors(res)
+
+
+class TestUpdateCategory(GraphQLTestCase):
+    GRAPHQL_SCHEMA = schema
+    fixtures = ['ingredients.json', ]
+    # GraphQLTestCase not working
+
+    def test_update(self):
+        res = self.query(query='''
+           mutation myMutation($name: String!) {
+                updateCategory(name: $name) {
+                    category {
+                        id
+                        name
+                    }
+                }
+            }
+            ''', variables="{\"name\": \"test\"}"
+
                          )
         self.assertResponseNoErrors(res)
--- cookbook/cookbook/ingredients/testIngredients_with_client.py
+++ cookbook/cookbook/ingredients/testIngredients_with_client.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-#from rest_framework.test import APIRequestFactory
+# from rest_framework.test import APIRequestFactory
 from django.test import TestCase, Client
 from django.contrib.auth.models import User, Group
 
@@ -24,6 +24,48 @@ class test_category(TestCase):
     def tearDown(self):
         pass
 
-    def test_get_snippet_detail(self):
-        res = self.client.get('/graphql',{"query":"{allCategories{pageInfo{hasNextPage}}}"}, format='json')
+    def test_get_allCategories_detail(self):
+        res = self.client.get(
+            '/graphql', {"query": "query getCategory {allCategories{pageInfo{hasNextPage}}}", "operationName": "getCategory"}, format='json')
         self.assertEqual(res.status_code, 200)
+
+    def test_mutate_categoryByForm(self):
+        res = self.client.post('/graphql', data={"query": '''
+            mutation myMutation {
+                            updateCategoryByForm(input: {name:"test123"}){
+                                category {
+                                    id
+                                    name
+                                }
+                            }
+                        }, ''', "operationName": "myMutation"}, format='json')
+        self.assertEqual(res.status_code, 200)
+    # sent by variable should work.
+
+    def test_mutate_categoryByForm_through_input(self):
+        res = self.client.post('/graphql', data={"query": '''
+            mutation myMutation($input: UpdateCategoryByFormInput!) {
+                            updateCategoryByForm(input: $input){
+                                category {
+                                    id
+                                    name
+                                }
+                            }
+                        }, ''', "operationName": "myMutation", "variables": "{\"input\":{\"name\": \"someValue\"}}"}, format='json')
+        self.assertEqual(res.status_code, 200)
+
+
+'''
+"input":"{\"name\": \"someValue\"}"
+
+'''
+
+
+def test_mutate_category_through_variable(self):
+    res = self.client.post('/graphql', data={"query": '''
+        mutation myMutation($name: String!) {updateCategory(name: $name) {
+                    category {
+                        id
+                        name}}}
+            , ''', "operationName": "myMutation", "variables": "{\"name\": \"someValue\"}"}, format='json')
+    self.assertEqual(res.status_code, 200)
