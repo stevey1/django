a876bd4d1a3090f743e376b0a89cdab79e17fdfa
steve1 <yang.fengsen@gmail.com>
2020-03-07 08:27:32 -0700

    add spaceX

--- cookbook/cookbook/schema.py
+++ cookbook/cookbook/schema.py
@@ -1,9 +1,10 @@
 from graphene import ObjectType, Schema, Field
 
 from cookbook.ingredients.schema import IngredientQuery, IngredientMutation
+from cookbook.spacex.schema import SpacexQuery
 
 
-class Query(IngredientQuery, ObjectType):
+class Query(IngredientQuery, SpacexQuery, ObjectType):
     # This class will inherit from multiple Queries
     # as we begin to add more apps to our project
     pass
---
+++ cookbook/cookbook/spacex/schema.py
@@ -0,0 +1,29 @@
+import graphene
+from  graphene import ObjectType,  Int, String, Boolean
+from graphene import relay
+from django.test import TestCase, Client
+class RocketType(ObjectType):
+    rocket_id = Int()
+    rocket_name = String()
+    rocket_type = String()
+    launch_year = String()
+
+class LaunchType(ObjectType):
+    flight_number = Int()
+    mission_name = String()
+    launch_year = String()
+    launch_date_local = String()
+    launch_success = Boolean()
+    rock = RocketType()
+    def resolve_flight_number(parent, info):
+        return f"{parent.first_name} {parent.last_name}"
+
+class SpacexQuery(graphene.ObjectType):
+    launches = graphene.List(LaunchType)
+    def resolve_launches(parent, info):
+        client = Client()
+        res = client.get(
+            'https://api.spacexdata.com/v3/launches',
+            format='json')
+        return res.data
+
